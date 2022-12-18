0f17ab442cb8fb9d79b649191b1b7a7bce84e494
Serena Gibbons <serenalgibbons@gmail.com>
2022-12-18 14:10:14 -0500

    Fix white space on mobile

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ function Home() {
     return (
         <section id="home" className="bg-darkblue text-white">
             <Container className="col-xxl-8 py-5">
-                <Row className="flex-lg-row-reverse align-items-center g-5 py-5">
+                <Row className="flex-row-reverse align-items-center py-5">
                     <Col md={4} className="mb-5 px-5">
                         <img src={avatar} class="rounded d-block mx-lg-auto img-fluid" alt="home img"/>
                     </Col>
